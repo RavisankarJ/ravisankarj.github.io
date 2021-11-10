402bcb4929e94a3c64c4e7d94f2abd549c7230a3
Ravisankar <ravi.jssr.208@gmail.com>
2021-11-10 14:20:06 +0530

    Updated by Ravi
    added all the necessary files.

---
+++ question_words.js
@@ -0,0 +1,77 @@
+levels = [
+    [
+        {word: 'a'},
+        {word: 'b'},
+        {word: 'c'},
+        {word: 'd'}
+    ],
+
+    [
+        {word: 'e'},
+        {word: 'f'},
+        {word: 'g'}
+        
+    ],
+
+    [
+        {word: 'h'},
+        {word: 'i'},
+        {word: 'j'},
+        {word: 'k'}
+    ],
+
+    [
+        {word: 'l'},
+        {word: 'm'},
+        {word: 'n'},
+        {word: 'o'},
+        {word: 'p'}
+    ],
+    
+    [
+        {word: 'q'},
+        {word: 'r'},
+        {word: 's'},
+        {word: 't'}
+    ],
+
+    [
+        {word: 'u'},
+        {word: 'v'},
+        {word: 'w'},
+        {word: 'x'},
+        {word: 'y'},
+        {word: 'z'}
+    ],
+
+    [
+        {word: 'a'},
+        {word: 'e'},
+        {word: 'i'},
+        {word: 'o'},
+        {word: 'u'}
+    ],
+
+    [
+        {word: 'b', font: 'cursive'},
+        {word: 'h', font: 'cursive'},
+        {word: 'k', font: 'cursive'},
+        {word: 'l', font: 'cursive'}
+    ],
+
+    [
+        {word: 'p', font: 'cursive'},
+        {word: 'q', font: 'cursive'},
+        {word: 'f', font: 'cursive'},
+        {word: 'y', font: 'cursive'},
+        {word: 'g', font: 'cursive'},
+        {word: 'j', font: 'cursive'}
+    ],
+
+    [
+        {word: 'b'},
+        {word: 'd'},
+        {word: 'p'},
+        {word: 'q'}
+    ]
+];
